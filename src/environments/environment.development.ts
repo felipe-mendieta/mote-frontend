c7b616d638cdf9b5579e3c8cfaf1614727f71cdc
Felipe Mendieta <felipe.mendieta@cedia.org.ec>
2024-04-22 19:41:46 -0500

    First setup for enviroment
    Config many enviroments variables.
    Prepare for production, complete not yet.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  mainUrl: 'http://localhost:3002/api/v1',
-  baseUrl: 'http://localhost:3002/api/v1',
-  socketUrl: 'http://localhost:3002',
+  mainUrl: 'http://localhost:3000/api/v1',
+  baseUrl: 'http://localhost:3000/api/v1',
+  socketUrl: 'http://localhost:3000',
   jsonURL: './../assets/jsons/poll.json',
   googleClientId:
     '705211840170-8oq3s9boqask2qnt4fuhhkustooksgfq.apps.googleusercontent.com',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,9 @@
+let basedomain: string = 'mote.ucuenca.edu.ec';
 export const environment = {
   production: true,
-  mainUrl: 'https://mote-engagement.netlify.app/#/auth/login',
-  baseUrl: 'https://backend-engagement-production.up.railway.app/api/v1',
-  socketUrl: 'https://backend-engagement-production.up.railway.app',
+  mainUrl: `${basedomain}/#/auth/login`,
+  baseUrl: `${basedomain}/api/v1`,
+  socketUrl: `${basedomain}`,
   jsonURL: './../assets/jsons/poll.json',
   googleClientId:
     '705211840170-8oq3s9boqask2qnt4fuhhkustooksgfq.apps.googleusercontent.com',
