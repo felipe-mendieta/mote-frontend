59d00548abe8544ce71ce261da3840d6dfc7e89a
lTIGREl <joefernando20899@gmail.com>
2024-07-15 03:11:32 -0500

    New dashboard
    New text on home-page buttons
    Emotions graphic finished
    Login page button view fixed

--- src/app/modules/admin/components/pages/dashboard/dashboard.component.ts
+++ src/app/modules/admin/components/pages/dashboard/dashboard.component.ts
@@ -2,6 +2,8 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription, debounceTime } from 'rxjs';
 import { LayoutService } from 'src/app/modules/admin/components/layout/service/app.layout.service';
 import { ChartModule } from 'primeng/chart';
+import { DataRealTimeService } from 'src/app/services/data-real-time.service';
+import { Emotion } from 'src/app/interfaces/emotion.interface';
 
 @Component({
   selector: 'app-dashboard',
@@ -34,7 +36,7 @@ export class DashboardComponent implements OnInit, OnDestroy {
 
   subscription: Subscription;
 
-  constructor(private layoutService: LayoutService) {
+  constructor(private layoutService: LayoutService, private dataRealTimeService: DataRealTimeService) {
     this.subscription = this.layoutService.configUpdate$
       .pipe(debounceTime(25))
       .subscribe((config) => {
@@ -44,6 +46,45 @@ export class DashboardComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.initCharts();
+    this.loadPreviousValues();
+  }
+
+  loadPreviousValues() {
+    this.dataRealTimeService
+      .getDashboardEmotions()
+      .pipe(/*tap((res) => console.log('tap in emotions chart logic get', res))*/)
+      .subscribe((data: Emotion) => {
+        if (data != null) {
+          this.updateChart(data);
+        } else {
+          //console.log('No "emotions" activity found.');
+        }
+      });
+  }
+
+  updateChart(newData: Emotion) {
+    const documentStyle = getComputedStyle(document.documentElement);
+    this.pieData = {
+      labels: ['Sorprendido', 'Asustado', 'Enfadado', 'Triste', 'Feliz'],
+      datasets: [
+        {
+          data: [newData.surprised, newData.afraid, newData.angry, newData.sad, newData.happy],
+          backgroundColor: [
+            documentStyle.getPropertyValue('--indigo-500'),
+            documentStyle.getPropertyValue('--purple-500'),
+            documentStyle.getPropertyValue('--black-500'),
+            documentStyle.getPropertyValue('--orange-500'),
+            documentStyle.getPropertyValue('--green-500')
+          ],
+          hoverBackgroundColor: [
+            documentStyle.getPropertyValue('--indigo-400'),
+            documentStyle.getPropertyValue('--purple-400'),
+            documentStyle.getPropertyValue('--black-400'),
+            documentStyle.getPropertyValue('--orange-400'), ,
+            documentStyle.getPropertyValue('--green-400')
+          ]
+        }]
+    };
   }
 
   initCharts() {
@@ -51,24 +92,6 @@ export class DashboardComponent implements OnInit, OnDestroy {
     const textColor = documentStyle.getPropertyValue('--text-color');
     const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
     const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
-
-    this.barData = {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-      datasets: [
-        {
-          label: 'My First dataset',
-          backgroundColor: documentStyle.getPropertyValue('--primary-500'),
-          borderColor: documentStyle.getPropertyValue('--primary-500'),
-          data: [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-          label: 'My Second dataset',
-          backgroundColor: documentStyle.getPropertyValue('--primary-200'),
-          borderColor: documentStyle.getPropertyValue('--primary-200'),
-          data: [28, 48, 40, 19, 86, 27, 90]
-        }
-      ]
-    };
 
     this.barOptions = {
       plugins: {
@@ -103,24 +126,6 @@ export class DashboardComponent implements OnInit, OnDestroy {
       }
     };
 
-    this.pieData = {
-      labels: ['A', 'B', 'C'],
-      datasets: [
-        {
-          data: [540, 325, 702],
-          backgroundColor: [
-            documentStyle.getPropertyValue('--indigo-500'),
-            documentStyle.getPropertyValue('--purple-500'),
-            documentStyle.getPropertyValue('--teal-500')
-          ],
-          hoverBackgroundColor: [
-            documentStyle.getPropertyValue('--indigo-400'),
-            documentStyle.getPropertyValue('--purple-400'),
-            documentStyle.getPropertyValue('--teal-400')
-          ]
-        }]
-    };
-
     this.pieOptions = {
       plugins: {
         legend: {
@@ -130,28 +135,6 @@ export class DashboardComponent implements OnInit, OnDestroy {
           }
         }
       }
-    };
-
-    this.lineData = {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-      datasets: [
-        {
-          label: 'First Dataset',
-          data: [65, 59, 80, 81, 56, 55, 40],
-          fill: false,
-          backgroundColor: documentStyle.getPropertyValue('--primary-500'),
-          borderColor: documentStyle.getPropertyValue('--primary-500'),
-          tension: .4
-        },
-        {
-          label: 'Second Dataset',
-          data: [28, 48, 40, 19, 86, 27, 90],
-          fill: false,
-          backgroundColor: documentStyle.getPropertyValue('--primary-200'),
-          borderColor: documentStyle.getPropertyValue('--primary-200'),
-          tension: .4
-        }
-      ]
     };
 
     this.lineOptions = {
@@ -181,91 +164,6 @@ export class DashboardComponent implements OnInit, OnDestroy {
             drawBorder: false
           }
         },
-      }
-    };
-
-    this.polarData = {
-      datasets: [{
-        data: [
-          11,
-          16,
-          7,
-          3
-        ],
-        backgroundColor: [
-          documentStyle.getPropertyValue('--indigo-500'),
-          documentStyle.getPropertyValue('--purple-500'),
-          documentStyle.getPropertyValue('--teal-500'),
-          documentStyle.getPropertyValue('--orange-500')
-        ],
-        label: 'My dataset'
-      }],
-      labels: [
-        'Indigo',
-        'Purple',
-        'Teal',
-        'Orange'
-      ]
-    };
-
-    this.polarOptions = {
-      plugins: {
-        legend: {
-          labels: {
-            color: textColor
-          }
-        }
-      },
-      scales: {
-        r: {
-          grid: {
-            color: surfaceBorder
-          }
-        }
-      }
-    };
-
-    this.radarData = {
-      labels: ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'],
-      datasets: [
-        {
-          label: 'My First dataset',
-          borderColor: documentStyle.getPropertyValue('--indigo-400'),
-          pointBackgroundColor: documentStyle.getPropertyValue('--indigo-400'),
-          pointBorderColor: documentStyle.getPropertyValue('--indigo-400'),
-          pointHoverBackgroundColor: textColor,
-          pointHoverBorderColor: documentStyle.getPropertyValue('--indigo-400'),
-          data: [65, 59, 90, 81, 56, 55, 40]
-        },
-        {
-          label: 'My Second dataset',
-          borderColor: documentStyle.getPropertyValue('--purple-400'),
-          pointBackgroundColor: documentStyle.getPropertyValue('--purple-400'),
-          pointBorderColor: documentStyle.getPropertyValue('--purple-400'),
-          pointHoverBackgroundColor: textColor,
-          pointHoverBorderColor: documentStyle.getPropertyValue('--purple-400'),
-          data: [28, 48, 40, 19, 96, 27, 100]
-        }
-      ]
-    };
-
-    this.radarOptions = {
-      plugins: {
-        legend: {
-          labels: {
-            color: textColor
-          }
-        }
-      },
-      scales: {
-        r: {
-          pointLabels: {
-            color: textColorSecondary,
-          },
-          grid: {
-            color: surfaceBorder
-          }
-        }
       }
     };
   }
